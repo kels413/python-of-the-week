507d54c9b3a4c980425b715d58a3d4b1559a86a8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-05 03:53:08 +0100

    a script that helps list your directory content

--- Lecture/list_directory.py
+++ Lecture/list_directory.py
@@ -1,3 +1,11 @@
 import os
 
-path = input("Please enter directory Path (absolute or relative) to view the output: g")
+path = input("Please enter directory Path (absolute or relative) to view the output: ")
+
+try:
+    expand_path = os.path.expanduser(path)
+    content = os.listdir(expand_path)
+except FileNotFoundError:
+    print("No such File or Directory")
+    
+
