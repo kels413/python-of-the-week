c6c6ed813e38f7cb90957307fc31f2819b25f638
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-05 04:48:41 +0100

    done with the task

--- Lecture/list_directory.py
+++ Lecture/list_directory.py
@@ -4,11 +4,11 @@ print("press ctr/command D to exit prompt")
 try:
     while True:
         path = input("Please enter directory Path (absolute or relative) to view the output: ")
-        if path == "~":
-            print("this is it")
-        if os.path.exists(path):
+        # check if the path exists and also a tilde so we expand
+        if os.path.exists(path) or path == "~":
             expand_path = os.path.expanduser(path)
             content = os.listdir(expand_path)
+            # Display Non-hidden file
             for cnt in content:
                 if not cnt.startswith("."):
                     print(cnt)
