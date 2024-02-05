9ef819fe84f5b03d4b8aa3fae4f7783d9d762d3f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-05 03:50:57 +0100

    testing

--- Lecture/main.py
+++ Lecture/main.py
@@ -4,7 +4,35 @@ import sys
 print(os.name)
 
 
+#testing for os.getcwd
+# cwd = os.getcwd()
+# print(cwd)
 
-cwd = os.getcwd()
 
-print(cwd)
+# #testing for os.chdir()
+
+# def check_directory():
+#     print("path before changing the directory")
+#     cwd = os.getcwd()
+
+
+# check_directory()
+# os.chdir('../')
+# print(os.getcwd())
+
+# path = "/Users/mistarkelly/ubunteu_shared/My-Projects/python-of-the-week/Lecture"
+
+# try:
+#     content = os.listdir(path)
+#     print(content)
+# except  FileNotFoundError:
+#    print("No such file")
+#     # pass
+   
+
+path = input("Enter directory to sort: ")
+
+expand_path = os.path.expanduser(path)
+
+print(os.listdir(expand_path))
+
