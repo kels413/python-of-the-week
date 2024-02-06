74753ade4524c491399994b0ef833622205a2a79
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-06 01:08:01 +0100

    finished the program

--- Lecture/list_directory.py
+++ Lecture/list_directory.py
@@ -10,9 +10,10 @@ print("press ctr/command D to exit prompt")
 try:
     while True:
         path = input("Please enter directory Path (absolute or relative) to view the output: ")
+        expand_path = os.path.expanduser(path)
         # check if the path exists and also a tilde so we expand
-        if os.path.exists(path) or path.startswith("~"):
-            expand_path = os.path.expanduser(path)
+        if os.path.exists(expand_path):
+            # check if path exit and is a directory
             if os.path.isdir(expand_path):
                 content = os.listdir(expand_path)
                 # Display Non-hidden file
@@ -20,8 +21,10 @@ try:
                     if not cnt.startswith("."):
                         print(cnt)
             else:
-                print("File not a Directory")
+                print("Error: The provided path is a file not a directory")
         else:
             print("No such Directory")
+        
 except (EOFError, KeyboardInterrupt):
     print("\nGood bye!")
+
