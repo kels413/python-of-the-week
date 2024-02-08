64a0d0bf44a9edd347c0457fc0927fabbd6ef563
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-08 14:10:48 +0100

    updated project

--- Lecture/py/project.py
+++ Lecture/py/project.py
@@ -3,7 +3,6 @@ A Python script for efficient file management on my system. The script should be
 """
 import os
 import shutil
-import magic
 
 class Organize:
 
@@ -14,41 +13,27 @@ class Organize:
 
     def recieve_userInput(self):
         user_input = input("Please Enter The Directory you wish to arrange: ")
-
-        expanded_path = os.path.expanduser(user_input)
+        self.expanded_path = os.path.expanduser(user_input)
 
         # check if file exists
-        if not os.path.exists(expanded_path):
-            raise FileNotFoundError("Error: File or directory does not exist")
-        
-        # check if its a valid directory and not a file
-        if os.path.isdir(expanded_path):
-            content = os.listdir(expanded_path)
-            # change current working the the path provided by the user
-            os.chdir(expanded_path)
-            print(content)
+        if not os.path.exists(self.expanded_path):
+            raise FileNotFoundError("File or directory does not exist")
+
+        # if valid directory and not a file:
+            # change cwd to user_input.
+    def change_directory(self):
+        if os.path.isdir(self.expanded_path):
+            # change current working to the path provided by the user
+            os.chdir(self.expanded_path)
         else:
-            print("Error: provided path is not a directory")
-
-    def create_directories(self):
-        """ create Directories
-            a method responsible for creating directories
-        """
-        try:
-            pass
-        except FileExistsError:
-            print("File aready created", self.__directories)
-
-    def move_files(self):
-        """Move files
-            A Method responsible for moving files into directories
-        """
-        pass
-
+            print("provided path is not a directory")
+       
+    
             
 try:      
     organize1 = Organize()
     organize1.recieve_userInput()
+    organize1.change_directory()
     organize1.create_directories()
 except FileNotFoundError as fe:
-    print(f"Erro:", fe)
+    print(f"Error:", fe)
