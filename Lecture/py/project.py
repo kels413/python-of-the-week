9743fa8046352670a85792d4b39babdc6c4f729f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-08 14:12:42 +0100

    created directories

--- Lecture/py/project.py
+++ Lecture/py/project.py
@@ -42,16 +42,12 @@ class Organize:
               #remove dot(.) from the ext using slicing.
               excluded_dot = ext[1:]
               if not os.path.isdir(excluded_dot):
-                    #os.mkdir(excluded_dot)
-                print(file)
-                
+                os.mkdir(excluded_dot)
 
-                
                 # os.mkdir(excluded_dot)
                 print("this is the excluded dot", excluded_dot)
                 print("Name of the file", name)
                 print("extension", ext)
-
 
         except FileExistsError:
             print("File aready created", self.__directories)
