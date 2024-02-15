930ca2cd1e37ee77cae1cc2448d4051cb3a71327
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:50:26 +0100

    extracted places tag

--- week2/task3.py
+++ week2/task3.py
@@ -1,9 +1,13 @@
 from task1 import soup
 """
-Task 4: Extract Place Information:
+Task 3: Extract Place Information:
 
 Extract information about each place, including the place name, price per night, maximum guests, number of rooms, number of bathrooms, owner, and description.
 """
 
+places = soup.find_all('article')
 
-print(soup)
+
+for place in places:
+    place_name = place.find('h2').text.strip()
+    print(place_name)
