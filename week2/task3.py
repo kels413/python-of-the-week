4da7d6ff1861a8d42b9038c1b30e3a293253f10e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 15:12:10 +0100

    use regex to trim whitespaces

--- week2/task3.py
+++ week2/task3.py
@@ -1,4 +1,5 @@
 from task1 import soup
+import re
 """
 Task 3: Extract Place Information:
 
@@ -10,8 +11,19 @@ places = soup.find_all('article')
 
 for place in places:
     place_name = place.find('h2').text.strip()
-    price_by_night = place.find('div', class_='price_by_night')
-    print(price_by_night)
-
-    
     print(place_name)
+    price_by_night = place.find('div', class_='price_by_night').text.strip()
+    print(price_by_night)
+    maximum_guest = place.find('div', class_='max_guest').text.strip()
+    print(maximum_guest)
+    no_of_rooms = place.find('div', class_='number_rooms').text.strip()
+    print(no_of_rooms)
+    no_of_bathrooms = place.find('div', class_='number_bathrooms').text.strip()
+    print(no_of_bathrooms)
+    owner = place.find('div', class_='owner').find('p').text
+    print(owner)
+    
+    description = place.find('div', class_='description').find('p').text.strip()
+    description = re.sub(r'\s+', '', description)
+
+    print(description)
