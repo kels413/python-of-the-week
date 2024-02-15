1fc55cb80d99a1c582d979149bd0266ef78c507a
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 15:52:49 +0100

    refactorization of code

--- week2/task3.py
+++ week2/task3.py
@@ -8,22 +8,29 @@ Extract information about each place, including the place name, price per night,
 
 places = soup.find_all('article')
 
+# for place in places:
+#     place_name = place.find('h2').text.strip()
+#     price_by_night = place.find('div', class_='price_by_night').text.strip()
+#     maximum_guest = place.find('div', class_='max_guest').text.strip()
+#     no_of_rooms = place.find('div', class_='number_rooms').text.strip()
+#     no_of_bathrooms = place.find('div', class_='number_bathrooms').text.strip()
+#     owner = place.find('div', class_='owner').find('p').text
+#     description = place.find('div', class_='description').find('p').text.strip()
+#     # description = re.sub(r'\s+', ' ', description)
+#     print(f"{owner}\nPlace-name:{place_name}\nprice-by-night:{price_by_night}\nmax-guest{maximum_guest}\nno-rooms: {no_of_rooms}\nno-bathrooms:{no_of_bathrooms}\ndescription:{description}")
+#     print()
 
+i = 0
+classes = ['price_by_night', 'max-guest', 'number_rooms', 'number_bathrooms', 'owner', 'description']
+storage = []
 for place in places:
-    place_name = place.find('h2').text.strip()
-    print(place_name)
-    price_by_night = place.find('div', class_='price_by_night').text.strip()
-    print(price_by_night)
-    maximum_guest = place.find('div', class_='max_guest').text.strip()
-    print(maximum_guest)
-    no_of_rooms = place.find('div', class_='number_rooms').text.strip()
-    print(no_of_rooms)
-    no_of_bathrooms = place.find('div', class_='number_bathrooms').text.strip()
-    print(no_of_bathrooms)
-    owner = place.find('div', class_='owner').find('p').text
-    print(owner)
+    classes[i] = place.find('div', place).text
     
-    description = place.find('div', class_='description').find('p').text.strip()
-    description = re.sub(r'\s+', '', description)
 
-    print(description)
+    # place_name =  place.find('div', classes[i])
+    # price_by_night = place.find('div', classes[i])
+    # max_guest =  place.find('div', classes[i])
+    i+=1
+    print(classes)
+   
+
