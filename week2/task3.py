9cf5641f35d523bb8e7743c8201ec609bf0aa3d1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:46:43 +0100

    task3

---
+++ week2/task3.py
@@ -0,0 +1,9 @@
+from task1 import soup
+"""
+Task 4: Extract Place Information:
+
+Extract information about each place, including the place name, price per night, maximum guests, number of rooms, number of bathrooms, owner, and description.
+"""
+
+
+print(soup)
