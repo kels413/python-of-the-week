87cf9832620f6e8f08463aa106dbf3390f5b6027
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:24:25 +0100

    extracted all the states in location div

--- week2/task1.py
+++ week2/task1.py
@@ -9,8 +9,7 @@ Extract the names of the states (Arizona, California) from the "locations" secti
 with open ('/Users/mistarkelly/ubuntu_shared/My-Projects/ALX-ONLY/AirBnB_clone/web_static/8-index.html','r') as file_html:
     content = file_html.read()
     soup = BeautifulSoup(content, 'lxml')
-    
     states_div = soup.find('div', class_='locations')
-    states = soup.find('h3')
-    print(states_div)
-    print(states)
+    states = soup.find('h3').text.strip()
+    states_names = soup.find('h4').text.strip()
+    print(f"all states in location div are {states}: {states_names}")
