6d15cf399305baa7814987957c31a7824b4f97ab
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:26:42 +0100

    updated the states

--- week2/task1.py
+++ week2/task1.py
@@ -10,6 +10,6 @@ with open ('/Users/mistarkelly/ubuntu_shared/My-Projects/ALX-ONLY/AirBnB_clone/w
     content = file_html.read()
     soup = BeautifulSoup(content, 'lxml')
     states_div = soup.find('div', class_='locations')
-    states = soup.find('h3').text.strip()
-    states_names = soup.find('h4').text.strip()
+    states = states_div.find('h3').text.strip()
+    states_names = states_div.find('h4').text.strip()
     print(f"all states in location div are {states}: {states_names}")
