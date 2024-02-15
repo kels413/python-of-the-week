da19a0430e054cc930a596213898740c77548edc
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:14:03 +0100

    scraped the prices of my website

--- week2/main.py
+++ week2/main.py
@@ -3,13 +3,12 @@ from bs4 import BeautifulSoup
 with open('/Users/mistarkelly/ubuntu_shared/My-Projects/ALX-ONLY/AirBnB_clone/web_static/8-index.html', 'r') as file_html:
     content = file_html.read()        
     soup = BeautifulSoup(content, 'lxml')
-    all_h1_tag = soup.find_all('h2')
+    articles  = soup.find_all('article')
 
-    for i in all_h1_tag:
-        print(i.text)
+
+    for article in articles:
+        hotel_name = article.find('h2').text.strip()
+        price_by_night = article.find('div', class_='price_by_night').text.strip()
+        print(f"<{hotel_name}> at night costs {price_by_night}")
+
         
-    # print(all_h1_tag)
-
-# create an instance of soup
-
-
