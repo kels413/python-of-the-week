946e7489fb7bd299d7e9492b22225c1a5c6101bd
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 13:26:06 +0100

    founded all h1 tag in html website

--- week2/main.py
+++ week2/main.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
 
-with open('/Users/mistarkelly/ubuntu_shared/My-Projects/ALX-ONLY/AirBnB_clone/web_static/8-index.html') as file_html:
-    content = file_html.read()
-    print(content)
+with open('/Users/mistarkelly/ubuntu_shared/My-Projects/ALX-ONLY/AirBnB_clone/web_static/8-index.html', 'r') as file_html:
+    content = file_html.read()        
+    soup = BeautifulSoup(content, 'lxml')
+    all_h1_tag = soup.find_all('h2')
+
+    for i in all_h1_tag:
+        print(i.text)
+        
+    # print(all_h1_tag)
+
+# create an instance of soup
+
+
