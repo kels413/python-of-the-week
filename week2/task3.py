5fa608868b329a3d15c31d311c7eeb702ab6f4d3
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-15 14:52:29 +0100

    extracted price by night

--- week2/task3.py
+++ week2/task3.py
@@ -10,4 +10,8 @@ places = soup.find_all('article')
 
 for place in places:
     place_name = place.find('h2').text.strip()
+    price_by_night = place.find('div', class_='price_by_night')
+    print(price_by_night)
+
+    
     print(place_name)
